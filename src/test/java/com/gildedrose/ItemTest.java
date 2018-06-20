a879aaaef496e2210d1578b188af2137cd3f5588
William Powell <william.powell92@gmail.com>
2018-06-20 15:41:43 +0100

    Tested toString method on Item

---
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemTest {
+  private Item item;
+
+  @Before
+  public void setup() {
+    item = new Item("name", 5, 10);
+  }
+
+  @Test
+  public void isStringCreatesCorrectString() {
+    assertEquals(item.toString(), "name, 5, 10");
+  }
+}
