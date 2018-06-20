d3ac1616d5df8ef604870fce145dfad00b4a01dd
William Powell <william.powell92@gmail.com>
2018-06-20 21:26:43 +0100

    Copied sellIn functionality from StandardItem to ReducingSellInItem

--- src/test/java/com/gildedrose/ReducingSellInItemTest.java
+++ src/test/java/com/gildedrose/ReducingSellInItemTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class ReducingSellInItemTest {
 
@@ -11,6 +12,21 @@ public class ReducingSellInItemTest {
   @Before
   public void setUp() {
     item = new ReducingSellInItem("An item", 10, 10);
+  }
+
+  @Test
+  public void updateSellInReducesSellInByOne() {
+    item.updateSellIn();
+
+    assertEquals(item.getSellIn(), 9);
+  }
+
+  @Test
+  public void updateSellInReducesDifferentSellInByOne() {
+    item.setSellIn(5);
+    item.updateSellIn();
+
+    assertEquals(item.getSellIn(), 4);
   }
 
 }
