165d2d4ce47d5ba9520eda98989ffc406d0c9629
William Powell <william.powell92@gmail.com>
2018-06-20 16:16:48 +0100

    Created brie class, extending item

---
+++ src/main/java/com/gildedrose/Brie.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class Brie extends Item {
+
+  public Brie(String name, int sellIn, int quality) {
+    super(name, sellIn, quality);
+  }
+}
---
+++ src/test/java/com/gildedrose/BrieTest.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+public class BrieTest {
+
+}
