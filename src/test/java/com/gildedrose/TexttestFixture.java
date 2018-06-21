2fd9e2213cb4951480de5813c8292a50e0bfc4c2
William Powell <william.powell92@gmail.com>
2018-06-21 17:27:36 +0100

    Inserted ConjuredItem into TexttestFixture

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -14,8 +14,7 @@ public class TexttestFixture {
       new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-      // conjured item does not work properly yet
-      new StandardItem("Conjured Mana Cake", 3, 6)};
+      new ConjuredItem("Conjured Mana Cake", 3, 6)};
 
     GildedRose app = new GildedRose(items);
 
