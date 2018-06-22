da459e43995cef2bbd999e5b4cf5052576d73100
William Powell <william.powell92@gmail.com>
2018-06-22 11:22:21 +0100

    Moved Brie constants into StandardItem

--- src/main/java/com/gildedrose/Brie.java
+++ src/main/java/com/gildedrose/Brie.java
@@ -2,9 +2,6 @@ package com.gildedrose;
 
 public class Brie extends StandardItem {
 
-  private static final int QUALITY_INCREMENT = 1;
-  private static final int BEFORE_SELL_BY_MODIFIER = 1;
-  private static final int PAST_SELL_BY_MODIFIER = 2;
   private static final int MAXIMUM_QUALITY = 50;
 
   public Brie(String name, int sellIn, int quality) {
--- src/main/java/com/gildedrose/StandardItem.java
+++ src/main/java/com/gildedrose/StandardItem.java
@@ -3,9 +3,9 @@ package com.gildedrose;
 public class StandardItem extends Item {
 
   private static final int MINIMUM_QUALITY = 0;
-  private static final int QUALITY_INCREMENT = 1;
-  private static final int BEFORE_SELL_BY_MODIFIER = 1;
-  private static final int PAST_SELL_BY_MODIFIER = 2;
+  final int QUALITY_INCREMENT = 1;
+  final int BEFORE_SELL_BY_MODIFIER = 1;
+  final int PAST_SELL_BY_MODIFIER = 2;
   private static final int SELL_IN_INCREMENT = 1;
 
   public StandardItem(String name, int sellIn, int quality) {
